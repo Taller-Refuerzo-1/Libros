672399eb59682d24cb28d4546b6d1239d66863c5
Natasha Nicole Navarro Nolasco <nicolenolas24@gmail.com>
2024-10-23 11:05:18 -0600

    se envian cambios de catlibro y kibro.

---
+++ Libros.API/Categoria.cs
@@ -0,0 +1,9 @@
+﻿namespace Libros.API
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string CatLibro { get; set; }
+    }
+}
---
+++ Libros.API/Libro.cs
@@ -0,0 +1,11 @@
+﻿namespace Libros.API
+{
+    public class Libro
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Autor { get; set; }
+        public string ISBM { get; set; }
+        public string Categoría { get; set; }
+    }
+}
